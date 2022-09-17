a9d35c8dcbca98ee7bee4609bc727d3f14bc89af
alfe <alfe10251@gmail.com>
2022-09-17 19:07:09 +0900

    add: import gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,16 +1,18 @@
+import gtag from 'ga-gtag';
+
 // See: https://github.com/zeit/next.js/blob/v9.4.4/examples/with-google-analytics/lib/gtag.js
 const GA_TRACKING_ID = process.env.GA_TRACKING_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 const pageview = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  gtag('config', GA_TRACKING_ID, {
     page_path: url,
   })
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 const event = ({ action, category, label, value }) => {
-  window.gtag('event', action, {
+  gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
