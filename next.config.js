c9b08fd87f3badccc1bd19c5a42ecd996ec8b76a
alfe <alfe10251@gmail.com>
2023-11-02 14:02:17 +0900

    upgrade dependencies (#7)
    * upgrade: minor dependencies
    
    * upgrade: dependencies major
    
    * upgrade: yarn and package.json

--- next.config.js
+++ next.config.js
@@ -3,13 +3,32 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'cdn-ak.f.st-hatena.com',
-      'cdn-ak2.f.st-hatena.com',
-      'lh3.googleusercontent.com',
-      'blog.alfebelow.com',
-      'opengraph.githubassets.com',
-      'localhost',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:'cdn-ak.f.st-hatena.com',
+      },
+      {
+        protocol: 'https',
+        hostname:'cdn-ak2.f.st-hatena.com',
+      },
+      {
+        protocol: 'https',
+        hostname:'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname:'blog.alfebelow.com',
+      },
+      {
+        protocol: 'https',
+        hostname:'opengraph.githubassets.com',
+      },
+      {
+        protocol: 'http',
+        port: '3000',
+        hostname:'localhost',
+      },
     ],
   },
   async redirects() {
