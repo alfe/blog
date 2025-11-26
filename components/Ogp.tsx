e3a5d71d0d6ac617dc8f1951aaf9ff99fc66424e
alfe <1934979+alfe@users.noreply.github.com>
2025-11-26 19:45:14 +0900

    add: 10 book from book shelf

--- components/Ogp.tsx
+++ components/Ogp.tsx
@@ -6,13 +6,13 @@ const Ogp = (props: { slug: string; title: string; description: string; thumbnai
 
   return (
     <Head>
-      <meta property="og:url" content={`https://blog.alfebelow.com/entry/${props.slug}`} />
+      <meta property="og:url" content={`${process.env.PRD_URL}/entry/${props.slug}`} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={props.title} />
       <meta property="og:description" content={props.description} />
       <meta property="og:site_name" content={siteTitle} />
       <meta property="og:locale" content="ja" />
-      <meta property="og:image" content={props.thumbnail} />
+      <meta property="og:image" content={`${process.env.PRD_URL}${props.thumbnail}`} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@alfe_below" />
     </Head>
