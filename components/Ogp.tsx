abccb5b7765da2a2b0192dd278b9ccd44c768603
alfe <1934979+alfe@users.noreply.github.com>
2025-11-30 19:12:30 +0900

    add: 202511book

--- components/Ogp.tsx
+++ components/Ogp.tsx
@@ -6,15 +6,23 @@ const Ogp = (props: { slug: string; title: string; description: string; thumbnai
 
   return (
     <Head>
-      <meta property="og:url" content={`${process.env.PRD_URL}/entry/${props.slug}`} />
-      <meta property="og:type" content="website" />
+      <meta property="description" content={props.description} />
       <meta property="og:title" content={props.title} />
       <meta property="og:description" content={props.description} />
       <meta property="og:site_name" content={siteTitle} />
-      <meta property="og:locale" content="ja" />
+      <meta property="og:url" content={`${process.env.PRD_URL}/entry/${props.slug}`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image:width" content="1365" />
+      <meta property="og:image:height" content="768" />
       <meta property="og:image" content={`${process.env.PRD_URL}${props.thumbnail}`} />
+      <meta property="og:image:alt" content={`${props.title}のサムネイル`} />
+      <meta property="og:locale" content="ja" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@alfe_below" />
+      <meta name="twitter:creator" content="@alfe_below" />
+      <meta name="twitter:title" content={props.title} />
+      <meta name="twitter:description" content={props.description} />
+      <meta name="twitter:image" content={`${process.env.PRD_URL}${props.thumbnail}`} />
     </Head>
   )
 }
